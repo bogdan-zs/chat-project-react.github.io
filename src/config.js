54d5f842e3aad73e2fd0548aebe6458107355b4b
Яaseka Bogdan <zaseka.bogdan@gmail.com>
2018-11-02 19:07:32 +0200

    init

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+let config = {
+    apiKey: "AIzaSyDwdVu82MGlwZk-cj5yJeC_aH8jd8peORA",
+    authDomain: "chat-9e391.firebaseapp.com",
+    databaseURL: "https://chat-9e391.firebaseio.com",
+    projectId: "chat-9e391",
+    storageBucket: "chat-9e391.appspot.com",
+    messagingSenderId: "508176079755"
+};
+
+firebase.initializeApp(config)
+
---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+import {createStore, applyMiddleware} from 'redux'
+
+export const store = createStore()
+
